aa0db41958af9cb76eb87ad7d9e2f8d314774b41
Hahin <nikolaygaginua@gmail.com>
2025-05-02 16:18:23 +0300

    hotfix

--- lib/src/bes_auth.dart
+++ lib/src/bes_auth.dart
@@ -74,42 +74,55 @@ class BesAuth {
 
   Future<BesSession> _getTokensWithCode(String code) async {
     final userAgent = await _generateUserAgent();
-    
+
     return await http.post(Uri.https(serviceUrl, GET_TOKENS_PATH), body: {
       "code": code,
       "client_id": clientId,
       "redirect_uri": redirectUri,
       "client_secret": clientSecret,
       "grant_type": "authorization_code",
-    }, headers: {'USER-AGENT': userAgent}).then((response) => BesSession.fromJson(response.body));
+    }, headers: {
+      'USER-AGENT': userAgent
+    }).then((response) => BesSession.fromJson(response.body));
   }
 
+  /// Повертає User-Agent виду:
+  /// "Mozilla/5.0 (<OSName> <OSVersion>; <DeviceModel>; DeviceID/<DeviceId>) <AppName>/<AppVersion>"
   Future<String> _generateUserAgent() async {
-    final PackageInfo packageInfo = await PackageInfo.fromPlatform();
-    final String appName = packageInfo.appName;
-    final String appVersion = packageInfo.version;
+    final pkg = await PackageInfo.fromPlatform();
+    final appName = pkg.appName;
+    final appVersion = pkg.version;
 
-    final String os = Platform.isAndroid
-        ? 'Android'
-        : Platform.isIOS
-            ? 'iOS'
-            : Platform.operatingSystem;
-    final String osVersion = Platform.operatingSystemVersion;
+    final deviceInfo = DeviceInfoPlugin();
 
-    final DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+    String osName;
+    String osVersion;
+    String deviceModel;
     String deviceId;
 
     if (Platform.isAndroid) {
-      final androidInfo = await deviceInfo.androidInfo;
-      deviceId = androidInfo.id;
+      final info = await deviceInfo.androidInfo;
+
+      osName = 'Android';
+      osVersion = info.version.release;
+      deviceModel = '${info.brand} ${info.model}'.trim();
+      deviceId = info.id;
     } else if (Platform.isIOS) {
-      final iosInfo = await deviceInfo.iosInfo;
-      deviceId = iosInfo.identifierForVendor ?? 'unknown';
+      final info = await deviceInfo.iosInfo;
+
+      osName = 'iOS';
+      osVersion = info.systemVersion;
+      deviceModel = info.utsname.machine;
+      deviceId = info.identifierForVendor ?? 'unknown';
     } else {
+      osName = Platform.operatingSystem;
+      osVersion = Platform.operatingSystemVersion;
+      deviceModel = Platform.localHostname;
       deviceId = Platform.localHostname;
     }
 
-    return '$appName/$appVersion '
-          '($os; $osVersion; DeviceID/$deviceId)';
+    // 3. Складаємо сам User-Agent
+    final comment = '$osName $osVersion; $deviceModel; DeviceID/$deviceId';
+    return 'Mozilla/5.0 ($comment) $appName/$appVersion';
   }
 }
