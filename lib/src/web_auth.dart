7bbdefa18f6dee482789040a3c8253e91094187a
Hahin <nikolaygaginua@gmail.com>
2025-05-02 17:23:24 +0300

    hotfix & optimisation

--- lib/src/web_auth.dart
+++ lib/src/web_auth.dart
@@ -11,68 +11,106 @@ class WebAuth {
   WebAuth({required this.redirectUri});
 
   Future<String> open(BuildContext context, String authUrl) async {
-    Completer completer = Completer<String>();
-    WebViewCookieManager().clearCookies();
+    final completer = Completer<String>();
 
     await showModalBottomSheet(
         context: context,
         isScrollControlled: true,
         backgroundColor: Colors.transparent,
-        builder: (context) => Scaffold(
-          resizeToAvoidBottomInset: false,
-            body: Stack(children: [
-              Align(
-                  alignment: Alignment.topCenter,
-                  child: Container(
-                    height: MediaQuery.of(context).size.height * 0.07,
-                    color: const Color(0xFF120338),
-                  )),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: SizedBox(
-                  height: MediaQuery.of(context).size.height * 0.94,
-                  child: WebViewWidget(
-                    controller: WebViewController()
-                      ..setJavaScriptMode(JavaScriptMode.unrestricted)
-                      ..clearCache()
-                      ..clearLocalStorage()
-                      ..setNavigationDelegate(NavigationDelegate(
-                        onNavigationRequest: (NavigationRequest request) {
-                          String actionOrigin = _getOriginUri(request.url);
-                          String redirectOrigin = _getOriginUri(redirectUri);
-
-                          if (actionOrigin == redirectOrigin) {
-                            Navigator.pop(context);
-                            completer.complete(request.url);
-                            return NavigationDecision.prevent;
-                          }
-
-                          return NavigationDecision.navigate;
-                      }))
-                      ..loadRequest(Uri.parse(authUrl)),
-                    gestureRecognizers: <
-                        Factory<OneSequenceGestureRecognizer>>{
-                      Factory<OneSequenceGestureRecognizer>(
-                        () => EagerGestureRecognizer(),
-                      ),
-                    },
-                  ),
-                ),
-              ),
-              Positioned(
-                  top: MediaQuery.of(context).size.height * 0.07,
-                  right: 10,
-                  child: InkWell(
-                    child: const Icon(Icons.close, color: Colors.white),
-                    onTap: () => Navigator.pop(context),
-                  )),
-            ]),
-          )).then((val) {
+        builder: (context) => WebAuthModal(authUrl: authUrl, redirectUri: redirectUri, completer: completer)).then((val) {
       if (!completer.isCompleted) {
         completer.complete('');
       }
     });
     return await completer.future;
+  }
+}
+
+class WebAuthModal extends StatefulWidget {
+  const WebAuthModal({
+    super.key,
+    required this.authUrl,
+    required this.redirectUri,
+    required this.completer,
+  });
+
+  final String authUrl;
+  final String redirectUri;
+  final Completer<String> completer;
+
+  @override
+  State<WebAuthModal> createState() => _WebAuthModalState();
+}
+
+class _WebAuthModalState extends State<WebAuthModal> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    WebViewCookieManager().clearCookies();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..clearCache()
+      ..clearLocalStorage()
+      ..setNavigationDelegate(NavigationDelegate(
+        onNavigationRequest: (NavigationRequest request) {
+          String actionOrigin = _getOriginUri(request.url);
+          String redirectOrigin = _getOriginUri(widget.redirectUri);
+
+          if (actionOrigin == redirectOrigin) {
+            Navigator.pop(context);
+            widget.completer.complete(request.url);
+            return NavigationDecision.prevent;
+          }
+
+          return NavigationDecision.navigate;
+      }))
+      ..loadRequest(Uri.parse(widget.authUrl));
+  }
+
+  @override
+  void dispose() {
+    
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+        body: Stack(children: [
+          Align(
+              alignment: Alignment.topCenter,
+              child: Container(
+                height: MediaQuery.of(context).size.height * 0.07,
+                color: const Color(0xFF120338),
+              )),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: SizedBox(
+              height: MediaQuery.of(context).size.height * 0.94,
+              child: WebViewWidget(
+                controller: _controller,
+                gestureRecognizers: <
+                    Factory<OneSequenceGestureRecognizer>>{
+                  Factory<OneSequenceGestureRecognizer>(
+                    () => EagerGestureRecognizer(),
+                  ),
+                },
+              ),
+            ),
+          ),
+          Positioned(
+              top: MediaQuery.of(context).size.height * 0.07,
+              right: 10,
+              child: InkWell(
+                child: const Icon(Icons.close, color: Colors.white),
+                onTap: () => Navigator.pop(context),
+              )),
+        ]),
+      );
   }
 
   String _getOriginUri(String uri) {
